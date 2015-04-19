76be833bf88e30cfc4c40afa389b6ba6a21471c0
David Freeman <daviddcbud@hotmail.com>
2015-04-19 17:07:48 -0500

    adjusting some tests

--- test.js
+++ test.js
@@ -3,27 +3,23 @@ var should=require('should');
       
 
 function setupRequestResponse(req,res) {
-req.body={};
-req.params={};
- req.params.id=1;
-res.send=function(s) {
-        };
-res.status=function(s){
-return res;
-           };
-
-
-
- 
+	req.body={};
+	req.params={};
+ 	req.params.id=1;
+	res.send=function(s){ };
+	res.status=function(s){
+		return res; //return itself for chaining
+      };
 }
 
-describe('Controller  Tests', function() {
+describe('Controller Tests', function() {
 
   it('should return items', function(){
      var Model={};
      var items=[];
+     //setup mock model to avoid real mongodb connections
      Model.find= function(id,callback){
-     	
+     	items.push({});
      	items.push({});
      	callback(items);
      };
@@ -35,7 +31,7 @@ describe('Controller  Tests', function() {
   
 	var spy1=sinon.spy(res,"send");
 	
-
+     //test that the get method sends the items
      controller.get(req,res);
      spy1.calledWith(items).should.be.equal(true);
      
@@ -43,53 +39,43 @@ describe('Controller  Tests', function() {
   });
   it('should return 500', function() {
       
- var Model = {};
+ 	var Model = {};
+ 	//setup the mock model that returns an error
     Model.findById= function(id, callback) {
       callback('error', null);
+		};
+   		var next=function(){};
+       	var req= {};
+      	var controller=require ('./controllers/controller')(Model);
+ 		var res={};
+    	setupRequestResponse(req,res);
+		var spy1=sinon.spy(res,"send");
+		var spy2=sinon.spy(res,"status");
 
-};
-   var next=function(){};
-       var req= {};
-      var controller=require ('./controllers/controller')(Model);
- var res={};
-    setupRequestResponse(req,res);
-
-   
-var spy1=sinon.spy(res,"send");
-var spy2=sinon.spy(res,"status");
-
-         controller.getById(req,res,next);
-      spy1.calledWith('error').should.be.equal(true);
-spy2.calledWith(500).should.be.equal(true);
+        controller.getById(req,res,next);
+      	spy1.calledWith('error').should.be.equal(true);
+		spy2.calledWith(500).should.be.equal(true);
 
 
 });
 
    it('should return 404', function() {
-
-
        var Model = {};
-    Model.findById= function(id, callback) {
-      callback(null, null);
-
+	    Model.findById= function(id, callback) {
+    	  callback(null, null);
 };
-   var next=function(){};
-       var req= {};
-      var controller=require ('./controllers/controller')(Model);
- var res={};
-    setupRequestResponse(req,res);
+   		var next=function(){};
+       	var req= {};
+      	var controller=require ('./controllers/controller')(Model);
+ 		var res={};
+    	setupRequestResponse(req,res);
 
-   
-var spy1=sinon.spy(res,"send");
-var spy2=sinon.spy(res,"status");
+		var spy1=sinon.spy(res,"send");
+		var spy2=sinon.spy(res,"status");
 
          controller.getById(req,res,next);
-      spy1.calledWith('not found').should.be.equal(true);
-spy2.calledWith(404).should.be.equal(true);
-
-
-             
-
+      	spy1.calledWith('not found').should.be.equal(true);
+		spy2.calledWith(404).should.be.equal(true);
 
 });
 
